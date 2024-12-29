7fbc8fda0a674da5d77c6f992f17442dafbcc508
michalBirenzwaig <michalostrov488@gmail.com>
2024-12-29 19:58:58 +0200

    Adding refresh page test

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -32,3 +32,14 @@ def test_access_check():
         expect(page).to_have_url('https://www.saucedemo.com/')
         expect(page.locator("[data-test='error']")).to_contain_text("You can only access '/inventory.html' when you are logged in.")
 
+def test_refresh_page():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=500)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto('https://www.saucedemo.com/')
+        page.locator('#user-name').fill("standard_user")
+        page.locator('#password').fill("secret_sauce")
+        page.get_by_role('button',name='Login').click()
+        page.reload(wait_until="load")
+        expect(page).to_have_url('https://www.saucedemo.com/inventory.html')
